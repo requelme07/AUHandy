32b3698523c3e9e614e9eb090e564838b427d5bd
unknown <ilabaz210191@gmail.com>
2021-11-22 14:37:23 +0700

    test mask

--- js/main.js
+++ js/main.js
@@ -26,13 +26,51 @@ let name = form.querySelector('.name')
 let mail = form.querySelector('.mail')
 let phone = form.querySelector('.phone')
 let password = form.querySelector('.password')
-let message = form.querySelector('.message')
+let spanName = form.querySelector('.spanName')
+let spanMail = form.querySelector('.spanMail')
+let spanPhone = form.querySelector('.spanPhone')
+let spanPass = form.querySelector('.spanPass')
+let spanMess = form.querySelector('.spanMess')
 
-form.addEventListener('submit', function(event) {
-    event.preventDefault()
-    console.log('name: ', name.value)
-    console.log('mail: ', mail.value)
-    console.log('phone: ', phone.value)
-    console.log('password: ', password.value)
-    console.log('message: ', message.value)
-})
+let regPhone = /^((\+7|7|8)+([0-9]){10})$/;
+let regMail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+let regMessage = /[A-Za-zA-Яа-яЁё]/;
+let regPass = /(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{6,}/g;
+
+
+btn.onclick = function(event) {
+    event.preventDefault();
+    if(!validate(regMessage, name.value)) {
+        notValid(name, spanName, "Введите имя")
+    } else {
+        valid(name, spanName, "")
+    }
+    if(!validate(regMail, mail.value)) {
+        notValid(mail, spanMail, "Некорректный email")
+    } else {
+        valid(mail, spanMail, "")
+    }
+    if(!validate(regPhone, phone.value)) {
+        notValid(phone, spanPhone, "Некорректный номер телефона")
+    } else {
+        valid(phone, spanPhone, "")
+    }
+    if(!validate(regPass, password.value)) {
+        notValid(password, spanPass, "Длина пароля должна быть не менее 6 символов, минимум одна цифра,буквенная часть пароля должна содержать как строчные так и прописные буквы")
+    } else {
+        valid(password, spanPass, "")
+    }
+}
+function validate (regex, inp) {
+    return regex.test(inp)
+}
+function notValid (inp, el, mess) {
+    inp.classList.add('is-invalid')
+    el.innerHTML = mess
+}
+function valid (inp, el, mess) {
+    inp.classList.remove('is-invalid')
+    inp.classList.add('is-valid')
+    el.innerHTML = mess
+}
+$(".phone").mask("+7(999)999-99-99");
