a6bbf9038c53cb519678af2d2cd00bd6c9817561
unknown <ilabaz210191@gmail.com>
2021-11-22 10:07:54 +0700

    added validate form

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-
+// Modal
 let dataModal = document.querySelector('[data-modal]')
 let modalId = document.getElementById('modal')
 let close = document.querySelector('[data-close]')
@@ -17,3 +17,22 @@ modalId.onclick = function() {
 modalDialog.onclick = function(event) {
     event.stopPropagation()
 }
+
+// Form validation
+
+let form = document.querySelector('.form_registration')
+let btn = form.querySelector('.btn')
+let name = form.querySelector('.name')
+let mail = form.querySelector('.mail')
+let phone = form.querySelector('.phone')
+let password = form.querySelector('.password')
+let message = form.querySelector('.message')
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault()
+    console.log('name: ', name.value)
+    console.log('mail: ', mail.value)
+    console.log('phone: ', phone.value)
+    console.log('password: ', password.value)
+    console.log('message: ', message.value)
+})
